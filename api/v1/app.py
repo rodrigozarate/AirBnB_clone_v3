b8a57e40a22e12c4d9f3d5284de5303da72e2516
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-02-05 14:37:58 -0500

    UPDATE: checking code

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import Flask
-from models import storage
+from models import *
+from models.__init__ import storage
 import os
 
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
-
-app = Flask (__name__)
-
-@app.register_blueprint(app_views)
 @app.teardown_appcontext
-def tearDown():
+def teardown_appcontext(exception):
     """Handles storage calls"""
     storage.close()
 
@@ -21,7 +20,7 @@ if __name__ == "__main__":
     HOST = os.getenv("HBNB_API_HOST")
    
     if PORT is None:
-        PORT =  5000
+        PORT = 5000
     if HOST is None:
         HOST = '0.0.0.0'
-    app.run(host= HOST, port=HOST)
+    app.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,7 @@
 from api.v1.views import app_views
 from flask import jsonify 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify(
-    status = "ok"
+        status="OK",
     )
-    
