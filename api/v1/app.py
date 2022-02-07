11c9cdeb494b6bb314d791fa3a8d9669b23fa84b
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-02-06 19:57:57 -0500

    ADD: Manage 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ API """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 from flask_cors import CORS
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Handles storage calls"""
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(err):
+    return jsonify({
+        "error": "Not found"
+    }), 404
 
 
 if __name__ == "__main__":
